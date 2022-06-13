cd7176ab4ac783d60a9b47cd9e510a4bed64f323
Sergey Dmitrievich <102258145+AngelicHedgehog@users.noreply.github.com>
2022-06-13 19:01:38 +0300

    Update Kruskal.go

--- Модуль 2/Kruskal.go
+++ Модуль 2/Kruskal.go
@@ -1,55 +1,63 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
+	"os"
+	"sort"
 )
 
-func main() {
-	var N, a, b int
-	_, _ = fmt.Scan(&N)
+/*
+	TIME LIMIT
+*/
 
-	cords := make([][2]int, N, N)
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	var N, a, b int
+	_, _ = fmt.Fscan(stdin, &N)
+
+	cords := make([][2]int, N)
 	for i := 0; i < N; i++ {
-		_, _ = fmt.Scan(&a, &b)
+		_, _ = fmt.Fscan(stdin, &a, &b)
 		cords[i] = [2]int{a, b}
 	}
 
-	NN := N * (N - 1) / 2
-	dots := make([][2]int, NN, NN)
-	edges := make([]int, NN, NN)
-
-	n := 0
+	var (
+		edges                    [][2]int
+		lenEdges, sortedLenEdges []int
+	)
 	for i := 0; i < N; i++ {
 		for j := i + 1; j < N; j++ {
-			dots[n] = [2]int{i, j}
-			a, b = cords[i][0]-cords[j][0], cords[i][1]-cords[j][1]
-			edges[n] = a*a + b*b
-			n++
-			for k := n - 1; k > 0; k-- {
-				if edges[k] < edges[k-1] {
-					dots[k], dots[k-1] = dots[k-1], dots[k]
-					edges[k], edges[k-1] = edges[k-1], edges[k]
-				}
-			}
+			edges = append(edges, [2]int{i, j})
+			a = (cords[i][0]-cords[j][0])*(cords[i][0]-cords[j][0]) + (cords[i][1]-cords[j][1])*(cords[i][1]-cords[j][1])
+			lenEdges = append(lenEdges, a)
+			sortedLenEdges = append(sortedLenEdges, a)
 		}
 	}
-	treeId := make([]int, 0)
-	for i := 0; i < N; i++ {
-		treeId = append(treeId, i)
-	}
-	var oldId, newId int
+	sort.Ints(sortedLenEdges)
+
 	dist := 0.0
-	for i := 0; i < NN; i++ {
-		if treeId[dots[i][0]] != treeId[dots[i][1]] {
-			dist += math.Sqrt(float64(edges[i]))
-			newId, oldId = treeId[dots[i][0]], treeId[dots[i][1]]
-			for j := 0; j < N; j++ {
-				if treeId[j] == oldId {
-					treeId[j] = newId
+	comps := make([]int, N)
+	for i := range comps {
+		comps[i] = i
+	}
+	for _, a = range sortedLenEdges {
+		for i := range lenEdges {
+			if a == lenEdges[i] {
+				if comps[edges[i][0]] != comps[edges[i][1]] {
+					dist += math.Sqrt(float64(a))
+					for j := 0; j < N; j++ {
+						if comps[j] == comps[edges[i][0]] && j != edges[i][0] {
+							comps[j] = comps[edges[i][1]]
+						}
+					}
+					comps[edges[i][0]] = comps[edges[i][1]]
 				}
+				lenEdges[i] = -1
 			}
 		}
 	}
+
 	fmt.Printf("%.2f", dist)
 }
