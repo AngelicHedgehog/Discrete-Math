39e99ec9c428c41233bbf7081730f9dcd9fa7045
Sergey Dmitrievich <102258145+AngelicHedgehog@users.noreply.github.com>
2022-06-17 14:32:24 +0300

    Update Kruskal.go

--- Модуль 2/Kruskal.go
+++ Модуль 2/Kruskal.go
@@ -5,59 +5,41 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"sort"
 )
-
-/*
-	TIME LIMIT
-*/
 
 func main() {
 	stdin := bufio.NewReader(os.Stdin)
-	var N, a, b int
+	var N, x, y, xy, a, min int
 	_, _ = fmt.Fscan(stdin, &N)
 
 	cords := make([][2]int, N)
 	for i := 0; i < N; i++ {
-		_, _ = fmt.Fscan(stdin, &a, &b)
-		cords[i] = [2]int{a, b}
+		_, _ = fmt.Fscan(stdin, &x, &y)
+		cords[i] = [2]int{x, y}
 	}
-
-	var (
-		edges                    [][2]int
-		lenEdges, sortedLenEdges []int
-	)
-	for i := 0; i < N; i++ {
-		for j := i + 1; j < N; j++ {
-			edges = append(edges, [2]int{i, j})
-			a = (cords[i][0]-cords[j][0])*(cords[i][0]-cords[j][0]) + (cords[i][1]-cords[j][1])*(cords[i][1]-cords[j][1])
-			lenEdges = append(lenEdges, a)
-			sortedLenEdges = append(sortedLenEdges, a)
-		}
-	}
-	sort.Ints(sortedLenEdges)
 
 	dist := 0.0
-	comps := make([]int, N)
-	for i := range comps {
-		comps[i] = i
+	edges := map[int]int{}
+	for i := 1; i < N; i++ {
+		x, y = cords[0][0]-cords[i][0], cords[0][1]-cords[i][1]
+		edges[i] = x*x + y*y
 	}
-	for _, a = range sortedLenEdges {
-		for i := range lenEdges {
-			if a == lenEdges[i] {
-				if comps[edges[i][0]] != comps[edges[i][1]] {
-					dist += math.Sqrt(float64(a))
-					for j := 0; j < N; j++ {
-						if comps[j] == comps[edges[i][0]] && j != edges[i][0] {
-							comps[j] = comps[edges[i][1]]
-						}
-					}
-					comps[edges[i][0]] = comps[edges[i][1]]
-				}
-				lenEdges[i] = -1
+	for len(edges) != 0 {
+		min = -1
+		for a = range edges {
+			if min == -1 || edges[min] > edges[a] {
+				min = a
+			}
+		}
+		dist += math.Sqrt(float64(edges[min]))
+		delete(edges, min)
+		for a = range edges {
+			x, y = cords[min][0]-cords[a][0], cords[min][1]-cords[a][1]
+			xy = x*x + y*y
+			if xy < edges[a] {
+				edges[a] = xy
 			}
 		}
 	}
-
 	fmt.Printf("%.2f", dist)
 }
