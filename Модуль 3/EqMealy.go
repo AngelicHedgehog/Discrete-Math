6551fe3613bd1a78f90677034c83977650d59b6d
Sergey Dmitrievich <102258145+AngelicHedgehog@users.noreply.github.com>
2022-06-17 14:38:29 +0300

    Update EqMealy.go

--- Модуль 3/EqMealy.go
+++ Модуль 3/EqMealy.go
@@ -1,64 +1,133 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
 
-func main() {
-	stdin := bufio.NewReader(os.Stdin)
-	var n, m, q0, a, i, j int
-	_, _ = fmt.Fscan(stdin, &n, &m, &q0)
+func minMealy() (int, [][]int, [][]string, []int, []int, int, [][]int, []int) {
+	var n, m, q0, i, j, k int
+	_, _ = fmt.Scan(&n, &m, &q0)
 	delta := make([][]int, n)
 	fi := make([][]string, n)
-	T := make([]int, n)
-	T_ := make([]int, n)
 	for i = 0; i < n; i++ {
-		T[i] = -1
 		delta[i] = make([]int, m)
 		for j = 0; j < m; j++ {
-			_, _ = fmt.Fscan(stdin, &delta[i][j])
+			_, _ = fmt.Scan(&delta[i][j])
 		}
 	}
 	for i = 0; i < n; i++ {
 		fi[i] = make([]string, m)
 		for j = 0; j < m; j++ {
-			_, _ = fmt.Fscan(stdin, &fi[i][j])
+			_, _ = fmt.Scan(&fi[i][j])
 		}
 	}
 
-	num := 0
-	queue := []int{q0}
-	lastElem := 0
-	for lastElem != -1 {
-		a = queue[lastElem]
-		queue = queue[:lastElem]
-		lastElem--
-		if T[a] == -1 {
-			T[a] = num
-			T_[num] = a
-			num++
-			for i = m - 1; i >= 0; i-- {
-				if T[delta[a][i]] == -1 {
-					queue = append(queue, delta[a][i])
-					lastElem++
+	groups := make([][]int, 0)
+	idToGroup := make([]int, n)
+	countGroups := 0
+	for i = 0; i < n; i++ {
+		for j = 0; j < countGroups; j++ {
+			for k = 0; k < m; k++ {
+				if fi[i][k] != fi[groups[j][0]][k] {
+					break
 				}
+			}
+			if k == m {
+				groups[j] = append(groups[j], i)
+				idToGroup[i] = j
+				break
+			}
+		}
+		if j == countGroups {
+			groups = append(groups, []int{i})
+			idToGroup[i] = countGroups
+			countGroups++
+		}
+	}
+
+	var (
+		countNewGroups, countSubGroups, groupElement int
+		newIdToGroup, group                          []int
+		newGroups, subGroups                         [][]int
+	)
+
+	for true {
+		newIdToGroup = make([]int, n)
+		newGroups = make([][]int, 0)
+		countNewGroups = 0
+		for _, group = range groups {
+			subGroups = make([][]int, 0)
+			countSubGroups = 0
+			for _, groupElement = range group {
+				for j = 0; j < countSubGroups; j++ {
+					for k = 0; k < m; k++ {
+						if idToGroup[delta[groupElement][k]] != idToGroup[delta[subGroups[j][0]][k]] {
+							break
+						}
+					}
+					if k == m {
+						subGroups[j] = append(subGroups[j], groupElement)
+						newIdToGroup[groupElement] = countNewGroups + j
+						break
+					}
+				}
+				if j == countSubGroups {
+					subGroups = append(subGroups, []int{groupElement})
+					newIdToGroup[groupElement] = countNewGroups + countSubGroups
+					countSubGroups++
+				}
+			}
+			newGroups = append(newGroups, subGroups...)
+			countNewGroups += countSubGroups
+		}
+		if countGroups == countNewGroups {
+			break
+		}
+		idToGroup = newIdToGroup
+		groups = newGroups
+		countGroups = countNewGroups
+	}
+
+	T := make([]int, countGroups)
+	T_ := make([]int, countGroups)
+	j = 0
+	mark := map[int]bool{}
+	queue := []int{q0}
+	lastInQueue := 0
+	for lastInQueue != -1 {
+		k = queue[lastInQueue]
+		queue = queue[:lastInQueue]
+		lastInQueue--
+		if mark[idToGroup[k]] {
+			continue
+		}
+		mark[idToGroup[k]] = true
+		T[j] = idToGroup[k]
+		T_[idToGroup[k]] = j
+		j++
+		for i = m - 1; i >= 0; i-- {
+			queue = append(queue, delta[k][i])
+			lastInQueue++
+		}
+	}
+	return m, delta, fi, T, T_, countGroups, groups, idToGroup
+}
+
+func main() {
+	m1, delta1, fi1, T1, T1_, countGroups1, groups1, idToGroup1 := minMealy()
+	m2, delta2, fi2, T2, T2_, countGroups2, groups2, idToGroup2 := minMealy()
+
+	if countGroups1 != countGroups2 || m1 != m2 {
+		fmt.Print("NOT EQUAL")
+		return
+	}
+	for i := 0; i < countGroups1; i++ {
+		for j := 0; j < m1; j++ {
+			if T1_[idToGroup1[delta1[groups1[T1[i]][0]][j]]] !=
+				T2_[idToGroup2[delta2[groups2[T2[i]][0]][j]]] ||
+				fi1[groups1[T1[i]][0]][j] != fi2[groups2[T2[i]][0]][j] {
+				fmt.Print("NOT EQUAL")
+				return
 			}
 		}
 	}
-
-	fmt.Printf("%d\n%d\n%d", n, m, 0)
-	for i = 0; i < n; i++ {
-		fmt.Println()
-		for j = 0; j < m; j++ {
-			fmt.Printf("%d ", T[delta[T_[i]][j]])
-		}
-	}
-	for i = 0; i < n; i++ {
-		fmt.Println()
-		for j = 0; j < m; j++ {
-			fmt.Printf("%s ", fi[T_[i]][j])
-		}
-	}
+	fmt.Print("EQUAL")
 }
