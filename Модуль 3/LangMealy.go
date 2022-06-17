0b3c82a3575e9db6a659947c25b4f29ad7eb47fc
Sergey Dmitrievich <102258145+AngelicHedgehog@users.noreply.github.com>
2022-06-17 14:39:12 +0300

    Update LangMealy.go

--- Модуль 3/LangMealy.go
+++ Модуль 3/LangMealy.go
@@ -2,47 +2,33 @@ package main
 
 import "fmt"
 
-func rec(word string, q, M int, nulls map[int]bool, dict *map[string]bool, delta *[][]int, fi *[][]string) {
-	if !(*dict)[word] {
-		(*dict)[word] = true
-		fmt.Printf("%s ", word)
-	}
-	var next int
-	if len(word) < M {
-		for i := 0; i < 2; i++ {
-			next = (*delta)[q][i]
-			if (*fi)[q][i] == "-" {
-				if !nulls[next] {
-					nulls[next] = true
-					rec(word, next, M, nulls, dict, delta, fi)
-					delete(nulls, next)
-				}
-			} else {
-				rec(word+(*fi)[q][i], next, M, map[int]bool{}, dict, delta, fi)
-			}
-		}
-	}
-}
-
 func main() {
-	var n, q0, M, i, j int
-	_, _ = fmt.Scan(&n)
+	var n, m, q0, i, j int
+	_, _ = fmt.Scan(&n, &m, &q0)
 	delta := make([][]int, n)
 	fi := make([][]string, n)
 	for i = 0; i < n; i++ {
-		delta[i] = make([]int, 2)
-		for j = 0; j < 2; j++ {
+		delta[i] = make([]int, m)
+		for j = 0; j < m; j++ {
 			_, _ = fmt.Scan(&delta[i][j])
 		}
 	}
 	for i = 0; i < n; i++ {
-		fi[i] = make([]string, 2)
-		for j = 0; j < 2; j++ {
+		fi[i] = make([]string, m)
+		for j = 0; j < m; j++ {
 			_, _ = fmt.Scan(&fi[i][j])
 		}
 	}
-	_, _ = fmt.Scan(&q0, &M)
+	alphas := make([]int, m)
+	for j = 0; j < m; j++ {
+		alphas[j] = 'a' + j
+	}
 
-	dict := map[string]bool{"": true}
-	rec("", q0, M, map[int]bool{}, &dict, &delta, &fi)
+	fmt.Print("digraph {\n\trankdir = LR")
+	for i = 0; i < n; i++ {
+		for j = 0; j < m; j++ {
+			fmt.Printf("\n\t%d -> %d [label = \"%c(%s)\"]", i, delta[i][j], alphas[j], fi[i][j])
+		}
+	}
+	fmt.Print("\n}")
 }
