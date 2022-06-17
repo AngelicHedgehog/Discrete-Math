28b84f13330255eb709a284ad627e4f72c1f025a
Sergey Dmitrievich <102258145+AngelicHedgehog@users.noreply.github.com>
2022-06-17 14:31:36 +0300

    Update EqDist.go

--- Модуль 2/EqDist.go
+++ Модуль 2/EqDist.go
@@ -1,21 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
-/*
-	ERROR IN ALGORITHM
-*/
+import "fmt"
 
 func main() {
 	var N, M, K, a, b int
 	_, _ = fmt.Scan(&N, &M)
 
-	graph := make([][]int, 0)
+	graph := make([][]int, N)
 	for i := 0; i < N; i++ {
-		graph = append(graph, make([]int, 0))
+		graph[i] = make([]int, 0)
 	}
 
 	for i := 0; i < M; i++ {
@@ -25,57 +18,45 @@ func main() {
 	}
 
 	_, _ = fmt.Scan(&K)
-	sups := make([]int, 0)
+	sups := make([]int, K)
 	for i := 0; i < K; i++ {
-		_, _ = fmt.Scan(&a)
-		sups = append(sups, a)
+		_, _ = fmt.Scan(&sups[i])
 	}
 
-	exists := make(map[int]bool)
-	for i := 0; i < N; i++ {
-		exists[i] = true
-	}
-	dists := make([]int, N, N)
 	var (
-		dots  []int
-		done  map[int]bool
-		queue []int
-		x     int
+		finDists map[int]int
+		dists    map[int]int
+		queue    []int
 	)
-	for i := 0; i < K; i++ {
-		dots = make([]int, N, N)
-		done = make(map[int]bool)
-		done[sups[i]] = true
-		queue = append(queue, sups[i])
+	for i, sup := range sups {
+		dists = map[int]int{sup: 1}
+		queue = []int{sup}
 		for len(queue) != 0 {
-			x = queue[0]
-			if exists[x] && dists[x] != dots[x] {
-				delete(exists, x)
+			for _, a = range graph[queue[0]] {
+				if dists[a] == 0 {
+					queue = append(queue, a)
+					dists[a] = dists[queue[0]] + 1
+				}
 			}
 			queue = queue[1:]
-			for _, b = range graph[x] {
-				if !done[b] {
-					done[b] = true
-					dots[b] = dots[x] + 1
-					if i == 0 {
-						dists[b] = dots[b]
-					}
-					queue = append(queue, b)
+		}
+		if i == 0 {
+			finDists = dists
+		} else {
+			for a = range finDists {
+				if finDists[a] != dists[a] {
+					delete(finDists, a)
 				}
 			}
 		}
 	}
-
-	if len(exists) == 0 {
-		fmt.Printf("-")
+	if len(finDists) == 0 {
+		fmt.Println("-")
 	} else {
-		queue = make([]int, 0, len(exists))
-		for a = range exists {
-			queue = append(queue, a)
-		}
-		sort.Ints(queue)
-		for _, a = range queue {
-			fmt.Printf("%d ", a)
+		for i := 0; i < N; i++ {
+			if finDists[i] != 0 {
+				fmt.Printf("%d ", i)
+			}
 		}
 	}
 }
